4206ca174d53f018d4e4e14c2ee27002975d3e81
MUSHUBO <shubo3132@gmail.com>
2025-08-23 05:55:17 +0600

    Hero section with beautiful banners has been added.

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 const Hero = () => {
     return (
-        <section className='py-24'>
-            
+        <section className="relative w-full">
+            <figure className="relative w-full h-[300px] md:h-[450px] lg:h-[620px]">
+                <Image
+                    src={"/assets/vegetable_banner.webp"}
+                    alt="Fresh vegetable banner"
+                    fill
+                    priority
+                    className="object-cover"
+                />
+
+                {/* Overlay Content */}
+                <div className="absolute inset-0 flex flex-col justify-center items-center md:items-start px-4 md:px-16 lg:px-32 text-center md:text-left space-y-8">
+                    <span className="text-orange-400 text-sm md:text-base lg:text-2xl font-light">
+                        Fresh Vegetables
+                    </span>
+                    <h1 className="flex flex-col gap-2 md:gap-4">
+                        <span className="text-2xl md:text-4xl lg:text-6xl font-bold">Organic Food</span>
+                        <span className="text-2xl md:text-4xl lg:text-6xl font-medium md:font-light">
+                            &amp; Healthy Tasty
+                        </span>
+                    </h1>
+                    <button className="btn btn-outline btn-sm md:btn-md lg:btn-lg">
+                        SHOP NOW
+                    </button>
+                </div>
+            </figure>
         </section>
     );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,30 +26,42 @@ const NavBar = () => {
 	}
 
 	return (
-		<div className='navbar rounded-lg w-11/12 mx-auto bg-gradient-to-tr from-green-50 to-green-100'>
-			<div className="navbar-start">
-				<div className="dropdown">
-					<div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-						<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+		<div className='rounded-lg bg-gradient-to-tr from-green-50 to-green-100'>
+			<div className='navbar w-11/12 mx-auto'>
+				<div className="navbar-start">
+					<div className="dropdown">
+						<div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+							<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+						</div>
+						<ul
+							tabIndex={0}
+							className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+							{nevMenu()}
+						</ul>
 					</div>
-					<ul
-						tabIndex={0}
-						className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+					<Link href={"/"} className='flex items-center justify-center ml-2 gap-2'>
+						<Image src={"/assets/logo.ico"} width={40} height={30} alt="Fresh Veggies Logo" />
+						<p className="text-xl font-bold invisible sm:visible">Fresh<span className='text-green-500'>Veggies</span></p>
+					</Link>
+				</div>
+				<div className="navbar-center hidden lg:flex ">
+					<ul className="menu menu-horizontal px-1 font-medium">
 						{nevMenu()}
 					</ul>
 				</div>
-				<Link href={"/"} className='flex items-center justify-center ml-2 gap-2'>
-					<Image src={"/assets/logo.ico"} width={40} height={30} alt="Fresh Veggies Logo" />
-					<p className="text-xl font-bold invisible sm:visible">Fresh<span className='text-green-500'>Veggies</span></p>
-				</Link>
-			</div>
-			<div className="navbar-center hidden lg:flex ">
-				<ul className="menu menu-horizontal px-1 font-medium">
-					{nevMenu()}
-				</ul>
-			</div>
-			<div className="navbar-end">
-				<a className="btn bg-green-400 hover:bg-green-500 rounded-md ">Login</a>
+				<div className="navbar-end flex items-center gap-3">
+					{/* avatar */}
+					<div className="tooltip tooltip-bottom">
+						<img
+							className='w-10 h-10 rounded-full object-cover cursor-pointer'
+							src={"/assets/avatar.png"}
+							alt=""
+						/>
+					</div>
+
+					{/* login button */}
+					<a className="btn bg-green-400 hover:bg-green-500 rounded-md ">Login</a>
+				</div>
 			</div>
 		</div>
 	);
